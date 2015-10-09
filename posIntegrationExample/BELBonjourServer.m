8f429cd3ef435badf288f96429bbe8696d7e8e98
Sean Wolter <sean@bellycard.com>
2015-10-09 15:05:55 -0500

    initial commit

---
+++ posIntegrationExample/BELBonjourServer.m
@@ -0,0 +1,96 @@
+//
+//  BELBonjourServer.m
+//  posIntegrationExample
+//
+//  Created by Sean Wolter on 9/28/15.
+//  Copyright © 2015 Belly. All rights reserved.
+//
+
+#import "BELBonjourServer.h"
+
+@import UIKit;
+
+@interface BELBonjourServer ()
+@property (nonatomic, copy) NSArray<DTBonjourDataConnection *> *approvedConnections;
+@end
+
+@implementation BELBonjourServer
+
+- (instancetype)initWithDisplayCode:(NSString *)displayCode {
+    // this string is the unique identifier for our merchant app
+    NSString *bonjourType = [NSString stringWithFormat:@"_bellypos_display_%@._tcp", displayCode];
+    self = [super initWithBonjourType:bonjourType];
+    if (self) {
+        _approvedConnections = @[];
+    }
+    return self;
+}
+
+- (void)connection:(DTBonjourDataConnection *)connection didReceiveObject:(id)object {
+    NSDictionary *jsonObject = [NSJSONSerialization JSONObjectWithData:object options:0 error:nil];
+
+    if ([self.approvedConnections containsObject:connection]) {
+        [self receivedPayload:jsonObject];
+    } else {
+        // if the sender is not already approved this should be a pairing request
+        NSDictionary *pairingRequest = jsonObject[@"pairingRequest"];
+        if (pairingRequest) {
+            [self connection:connection receivedPairingRequest:pairingRequest];
+        }
+    }
+}
+
+- (void)connection:(DTBonjourDataConnection *)connection receivedPairingRequest:(NSDictionary *)pairingRequest {
+    if ([self.approvedConnections containsObject:connection]) {
+        // already added this connection
+        return;
+    }
+    NSString *deviceID = pairingRequest[@"deviceIdentifier"];
+    NSString *deviceName = pairingRequest[@"deviceName"];
+    NSString *message = [NSString stringWithFormat:@"A %@ named %@ wants to connect with you.", deviceName, deviceID];
+    // In the real app you should probably allow the user to deny the connection
+    [[[UIAlertView alloc] initWithTitle:@"Connecting!" //I know this is deprecated. This is just a proof of concept. :P
+                                message:message
+                               delegate:nil
+                      cancelButtonTitle:@"OK"
+                      otherButtonTitles:nil] show];
+    self.approvedConnections = [self.approvedConnections arrayByAddingObject:connection];
+    // if the user denied the connection in production
+    // you would say [connection close] and wait for the next request
+
+    // since this request to connect is "approved" send the configuration next
+    [self sendConfiguration];
+}
+
+- (void)sendConfiguration {
+    // This is hard coded for this example only
+    NSDictionary *config = @{
+                             @"config" : @{
+                                     @"cashier_name" : @"Sean",
+                                     @"register_name" : @"A Register",
+                                     @"enableLoyalty" : @1,
+                                     @"outlet_name" : @"An Outlet",
+                                     @"retailer_name" : @"The Best Store Ever",
+                                     @"displayRetailPriceTaxInclusive" : @1,
+                                     }
+                             };
+    [self sendPayload:config];
+}
+
+- (void)receivedPayload:(NSDictionary *)payload {
+    NSLog(@"received payload/n%@", payload);
+}
+
+- (BOOL)sendPayload:(NSDictionary *)payload {
+    NSError *error;
+    BOOL success = YES;
+    for (DTBonjourDataConnection *connection in self.approvedConnections) {
+        success = success && [connection sendObject:payload error:&error];
+        if (!success) {
+            NSLog(@"ERROR SENDING PAYLOAD!/n%@", error);
+        }
+    }
+    return success;
+}
+
+@end
---
+++ posIntegrationExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  posIntegrationExample
+//
+//  Created by Sean Wolter on 9/28/15.
+//  Copyright © 2015 Belly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
